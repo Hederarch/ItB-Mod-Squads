794b2271bd9f1bef9c491c84a7efe08ab0ad2072
Hederarch <66890769+Hederarch@users.noreply.github.com>
2022-10-11 16:06:09 -0500

    Version 1.1
    now uses ModApiExt 1.15!

--- Hedera's Squad Remix/scripts/init.lua
+++ Hedera's Squad Remix/scripts/init.lua
@@ -1,6 +1,6 @@
 return {
 	id = "hedera_squads",
-	name = "Hedera's Squad Remix",
+	name = "Hedera's Squad Remix 1.1",
 	description = "4 unique Squads based on the corporate islands.",
 	icon = "scripts/icon.png",
 	submodFolders = {"mods/"},
